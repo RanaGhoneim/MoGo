69f1d9b228aba46818019b295572a6bd55a39ab7
RanaGhoneim <80163538+RanaGhoneim@users.noreply.github.com>
2021-05-04 13:00:45 +0200

    new

--- MOGO/js/index.js
+++ MOGO/js/index.js
@@ -1,6 +1,5 @@
 window.addEventListener('scroll' , function(){
     let nav=document.getElementById("nav");
-    let windowPosition= window.scrollY>230;
-    console.log(window.scrollY)
+    let windowPosition= window.scrollY>100;
     nav.classList.toggle('navbarlist',windowPosition);
 })
